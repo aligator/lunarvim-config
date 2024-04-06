f949bdebc0e21159ac2a659326f7b087ec8df652
aligator <me@aligator.dev>
2024-04-06 12:50:44 +0200

    Use rustaceanvim for more easy rust configuration & replace auto save plugin

--- config.lua
+++ config.lua
@@ -16,12 +16,19 @@ lvim.builtin.treesitter.ensure_installed = {
 }
 
 lvim.format_on_save.enabled = true
+lvim.format_on_save = true
 lvim.number = true
+
+lvim.builtin.which_key.mappings["."] = {
+  "<cmd>lua vim.lsp.buf.code_action({apply = true})<cr>", "Code Actions"
+}
 
 table.insert(lvim.plugins, {
 
   -- copilot
   "zbirenbaum/copilot-cmp",
+  enabled = false,
+
   event = "InsertEnter",
   dependencies = { "zbirenbaum/copilot.lua" },
   config = function()
@@ -59,48 +66,35 @@ table.insert(lvim.plugins, {
 })
 
 -- auto save
-table.insert(lvim.plugins, {
-  "pocco81/auto-save.nvim",
-  config = function()
-    return {
-      enabled = true,        -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-      execution_message = {
-        message = function() -- message to print on save
-          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-        end,
-        dim = 0.18,                                      -- dim the color of `message`
-        cleaning_interval = 1250,                        -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-      },
-      trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-      -- function that determines whether to save the current buffer or not
-      -- return true: if buffer is ok to be saved
-      -- return false: if it's not ok to be saved
-      condition = function(buf)
-        local fn = vim.fn
-        local utils = require("auto-save.utils.data")
 
-        if
-            fn.getbufvar(buf, "&modifiable") == 1 and
-            utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-          return true                -- met condition(s), can save
-        end
-        return false                 -- can't save
-      end,
-      write_all_buffers = false,     -- write all buffers when the current one meets `condition`
-      debounce_delay = 135,          -- saves the file at most every `debounce_delay` milliseconds
-      callbacks = {                  -- functions to be executed at different intervals
-        enabling = nil,              -- ran when enabling auto-save
-        disabling = nil,             -- ran when disabling auto-save
-        before_asserting_save = nil, -- ran before checking `condition`
-        before_saving = nil,         -- ran before doing the actual save
-        after_saving = nil           -- ran after doing the actual save
-      }
-    }
-  end
+-- This plugin is more used, but it triggers a reformat even on just typing somehow...
+-- table.insert(lvim.plugins, {
+--   "okuuva/auto-save.nvim",
+--   enabled = false,
+--   opts = {
+--     -- your config goes here
+--     -- or just leave it empty :)
+--   },
+-- })
+
+-- So for now use this simpler one.
+table.insert(lvim.plugins, {
+  "aidenlangley/auto-save.nvim",
+  event = { "BufReadPre" },
+  opts = {
+    events = { 'InsertLeave', 'BufLeave' },
+    exclude_ft = { 'neo-tree' },
+    save_cmd = "w!",
+    timeout = 200 -- let the formatter run
+  },
 })
 
--- Rust config
--- https://github.com/LunarVim/starter.lvim/blob/rust-ide/config.lua
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
+table.insert(lvim.plugins, {
+  'mrcjkb/rustaceanvim',
+  version = '^4', -- Recommended
+  ft = { 'rust' },
+})
 table.insert(lvim.plugins, {
   "simrat39/rust-tools.nvim",
   {
@@ -118,123 +112,22 @@ table.insert(lvim.plugins, {
         },
       }
     end,
-  },
-  {
-    "j-hui/fidget.nvim",
-    config = function()
-      require("fidget").setup()
-    end,
-  },
+  }
 })
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
-
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-
-local codelldb_path = mason_path .. "bin/codelldb"
-local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
-local this_os = vim.loop.os_uname().sysname
-
--- The path in windows is different
-if this_os:find "Windows" then
-  codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
-  liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
-else
-  -- The liblldb extension is .so for linux and .dylib for macOS
-  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-end
-
-pcall(function()
-  require("rust-tools").setup {
-    tools = {
-      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-      reload_workspace_from_cargo_toml = true,
-      runnables = {
-        use_telescope = true,
-      },
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = false,
-        parameter_hints_prefix = "<-",
-        other_hints_prefix = "=>",
-        max_len_align = false,
-        max_len_align_padding = 1,
-        right_align = false,
-        right_align_padding = 7,
-        highlight = "Comment",
-      },
-      hover_actions = {
-        border = "rounded",
-      },
-      on_initialized = function()
-        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-          pattern = { "*.rs" },
-          callback = function()
-            local _, _ = pcall(vim.lsp.codelens.refresh)
-          end,
-        })
-      end,
-    },
-    dap = {
-      -- adapter= codelldb_adapter,
-      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-    },
-    server = {
-      on_attach = function(client, bufnr)
-        require("lvim.lsp").common_on_attach(client, bufnr)
-        local rt = require "rust-tools"
-        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-      end,
-
-      capabilities = require("lvim.lsp").common_capabilities(),
-      settings = {
-        ["rust-analyzer"] = {
-          lens = {
-            enable = true,
-          },
-          checkOnSave = {
-            enable = true,
-            command = "clippy",
-          },
-        },
-      },
-    },
-  }
-end)
-
-lvim.builtin.dap.on_config_done = function(dap)
-  dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-  dap.configurations.rust = {
-    {
-      name = "Launch file",
-      type = "codelldb",
-      request = "launch",
-      program = function()
-        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-      end,
-      cwd = "${workspaceFolder}",
-      stopOnEntry = false,
-    },
-  }
-end
-
-vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustLsp externalDocs<Cr>", { noremap = true, silent = true })
 
 lvim.builtin.which_key.mappings["r"] = {
   name = "Rust",
-  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+  r = { "<cmd>RustLsp runnables<Cr>", "Runnables" },
   t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-  R = {
-    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-    "Reload Workspace",
-  },
-  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  m = { "<cmd>RustLsp expandMacro<Cr>", "Expand Macro" },
+  c = { "<cmd>RustLsp openCargo<Cr>", "Open Cargo" },
+  p = { "<cmd>RustLsp parentModule<Cr>", "Parent Module" },
+  d = { "<cmd>RustLsp debuggables<Cr>", "Debuggables" },
+  v = { "<cmd>RustLsp crateGraph<Cr>", "View Crate Graph" },
+  R = { "<cmd>RustLsp reloadWorkspace<Cr>", "Reload Workspace" },
+  o = { "<cmd>RustLsp externalDocs<Cr>", "Open External Docs" },
   y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
   P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
   i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
@@ -242,6 +135,150 @@ lvim.builtin.which_key.mappings["r"] = {
   D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
 }
 
-lvim.builtin.which_key.mappings["."] = {
-  "<cmd>lua vim.lsp.buf.code_action({apply = true})<cr>", "Code Actions"
-}
+-- -- alternative Rust config with the rust-tools
+-- -- https://github.com/LunarVim/starter.lvim/blob/rust-ide/config.lua
+-- table.insert(lvim.plugins, {
+--   "simrat39/rust-tools.nvim",
+--   {
+--     "saecki/crates.nvim",
+--     version = "v0.3.0",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function()
+--       require("crates").setup {
+--         null_ls = {
+--           enabled = true,
+--           name = "crates.nvim",
+--         },
+--         popup = {
+--           border = "rounded",
+--         },
+--       }
+--     end,
+--   },
+--   {
+--     "j-hui/fidget.nvim",
+--     config = function()
+--       require("fidget").setup()
+--     end,
+--   },
+-- })
+
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
+
+-- local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+
+-- local codelldb_path = mason_path .. "bin/codelldb"
+-- local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
+-- local this_os = vim.loop.os_uname().sysname
+
+-- -- The path in windows is different
+-- if this_os:find "Windows" then
+--   codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+--   liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
+-- else
+--   -- The liblldb extension is .so for linux and .dylib for macOS
+--   liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+-- end
+
+-- pcall(function()
+--   require("rust-tools").setup {
+--     tools = {
+--       executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+--       reload_workspace_from_cargo_toml = true,
+--       runnables = {
+--         use_telescope = true,
+--       },
+--       inlay_hints = {
+--         auto = true,
+--         only_current_line = false,
+--         show_parameter_hints = false,
+--         parameter_hints_prefix = "<-",
+--         other_hints_prefix = "=>",
+--         max_len_align = false,
+--         max_len_align_padding = 1,
+--         right_align = false,
+--         right_align_padding = 7,
+--         highlight = "Comment",
+--       },
+--       hover_actions = {
+--         border = "rounded",
+--       },
+--       on_initialized = function()
+--         vim.api.nvim_create_autocmd({
+--           "BufWritePost",
+--           "BufEnter",
+--           "CursorHold",
+--           "InsertLeave"
+--         }, {
+--           pattern = { "*.rs" },
+--           callback = function()
+--             local _, _ = pcall(vim.lsp.codelens.refresh)
+--           end,
+--         })
+--       end,
+--     },
+--     dap = {
+--       -- adapter= codelldb_adapter,
+--       adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+--     },
+--     server = {
+--       on_attach = function(client, bufnr)
+--         require("lvim.lsp").common_on_attach(client, bufnr)
+--         local rt = require "rust-tools"
+--         vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+--       end,
+
+--       capabilities = require("lvim.lsp").common_capabilities(),
+--       settings = {
+--         ["rust-analyzer"] = {
+--           lens = {
+--             enable = true,
+--           },
+--           checkOnSave = {
+--             enable = true,
+--             command = "clippy",
+--           },
+--         },
+--       },
+--     },
+--   }
+-- end)
+
+-- lvim.builtin.dap.on_config_done = function(dap)
+--   dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+--   dap.configurations.rust = {
+--     {
+--       name = "Launch file",
+--       type = "codelldb",
+--       request = "launch",
+--       program = function()
+--         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+--       end,
+--       cwd = "${workspaceFolder}",
+--       stopOnEntry = false,
+--     },
+--   }
+-- end
+
+-- vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+
+-- lvim.builtin.which_key.mappings["r"] = {
+--   name = "Rust",
+--   r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+--   t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+--   m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+--   c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+--   p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+--   d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+--   v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+--   R = {
+--     "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+--     "Reload Workspace",
+--   },
+--   o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+--   y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+--   P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+--   i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+--   f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+--   D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+-- }
